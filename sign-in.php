0f4303fba31f06cd450151c67956259351772c61
emrebasturmen <ebasturmem@outlook.com>
2020-07-11 09:21:33 +0300

    The necessary libraries were added for the project and the homepage designed.

---
+++ sign-in.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/sign.css">
+    <title>Alaaddin Adworks Challenge</title>
+</head>
+<body class="text-center">
+<form class="form-sign" id="signin-form" action="includes/login.inc.php" method="post">
+    <h1 class="h3 mb-3 font-weight-normal">Kullanıcı Girişi</h1>
+    <input type="email" name="email" id="email" class="form-control first" placeholder="E-posta" required autofocus>
+    <input type="password" name="password" id="password" class="form-control" placeholder="Şifre" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Giriş Yap</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+</form>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ sign-up.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/sign.css">
+    <title>Alaaddin Adworks Challenge</title>
+</head>
+<body class="text-center">
+<form class="form-sign">
+    <h1 class="h3 mb-3 font-weight-normal">Kullanıcı Kayıt</h1>
+
+    <input type="text" id="name" class="form-control first" placeholder="Ad" required autofocus>
+    <input type="text" id="inputEmail" class="form-control" placeholder="Soyad" required>
+    <input type="text" id="inputEmail" class="form-control" placeholder="Telefon" required>
+    <input type="email" id="inputEmail" class="form-control" placeholder="E-posta" required >
+    <input type="password" id="inputPassword" class="form-control" placeholder="Şifre" required>
+
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Giriş Yap</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+</form>
+</body>
+</html>
